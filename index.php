62d1c5dbbfbbe0ad012f7e9e1a850b2b5b4c05fb
nguyen2906 <nguyenthinhuy290600@gmail.com>
2022-03-09 09:17:48 +0700

    sua textbox

--- index.php
+++ index.php
@@ -13,13 +13,13 @@
         <div class="form-group">
             <i class="far fa-user"></i>
             <label>tên đăng nhập</label>
-            <input type="text" class="form-input" id="txtUserName" name="username" placeholder="Tên đăng nhập">
+            <input type="text" class="form-input" id="textboxusername" name="username" placeholder="Tên đăng nhập">
         </div>
 
         <div class="form-group">
             <i class="far fa-key"></i>
             <label>mật khẩu</label>
-            <input type="password" class="form-input" id="txtpassword" name="password" placeholder="Mật khẩu">
+            <input type="password" class="form-input" id="textboxpassword" name="password" placeholder="Mật khẩu">
         </div>
         <input type="submit" name="submit" value="đăng nhập" class="form-submit">
     </form>
